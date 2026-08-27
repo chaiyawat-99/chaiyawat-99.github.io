1d6e57cff4f5e99f5b06f489d2e02649395adf93
Nack99 <chaiyawat076@gmail.com>
2026-08-27 13:34:40 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 doucument.addEventListener('DOMContentLoaded',funtion () {
+     document.getElementById('year').innerHTML = new Date().getFullYear();
      console.log('Portfolio ของ CHAIYAWAT พร้อมแล้ว!);
 });
                  
